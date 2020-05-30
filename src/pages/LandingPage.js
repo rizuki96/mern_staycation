a405702e3bcd022080d54e1c01adee12935cdbd6
Rizky <rizkyfirmansyah967@gmail.com>
2020-05-30 11:20:14 +0700

    styling Header parts

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
-import Button from 'elements/Button';
+import Header from 'parts/Header';
 
 export default class LandingPage extends Component {
   render() {
     return (
-      <div />
+      <>
+        <Header {...this.props} />
+      </>
     );
   }
 }
